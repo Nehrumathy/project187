ecf38064165dee2b52e73d5d371bb5dc55fcc751
Nehrumathy <rajmathi456@gmail.com>
2016-06-08 11:08:37 +0530

    project

---
+++ weekday.c
@@ -0,0 +1,28 @@
+# project187
+#include<stdio.h>
+
+int main()
+{
+int n;
+//to clear the screen
+printf("1.Sunday 2. Monday  3.Tuesday  4. Wednesday   5.Thursday  6.Friday  7.Saturday\n");
+scanf("%d",&n);
+
+switch(n)
+{
+case 1: printf("False");
+break;
+case 2: printf("True");
+break;
+case 3: printf("True");
+break;
+case 4: printf("True");
+break;
+case 5: printf("True");
+break;
+case 6: printf("True");
+break;
+case 7: printf("False");
+break;
+} //to stop the screen
+}
